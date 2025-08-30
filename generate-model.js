296d9cadf56971dace00203e16a73bff32de618b
Dezhen Kong <timkong@zju.edu.cn>
2025-08-31 01:06:50 +0800

    Update

--- generate-model.js
+++ generate-model.js
@@ -2,49 +2,54 @@
 
 const deepEqual = require('deep-equal');
 const yargs = require('yargs/yargs')
-const {hideBin} = require('yargs/helpers')
+const { hideBin } = require('yargs/helpers')
 const fs = require('node:fs');
-const {getType, getArgumentType, isCovariant, makeRandomString} = require('./lib/utils');
-const process = require('process');
+const vm = require('node:vm');
+const { getType, getArgumentType, isCovariant, makeRandomString } = require('./lib/utils');
+const process = require('node:process');
 const path = require('node:path');
 const objectHash = require('object-hash');
+const babelParser = require('@babel/parser');
+const babelGenerator = require('@babel/generator').default;
+const babelTraverse = require('@babel/traverse').default;
 const pretty = require('pino-pretty');
 const pino = require('pino').default;
 const assert = require('assert');
-const logger = pino(pretty({sync: true}));
+const logger = pino(pretty({ sync: true }));
 
 let argv = yargs(hideBin(process.argv))
-  .usage('Generate an API model for a library based on the given client code').option('library', {
-    alias: 'l',
-    type: 'string',
-    description: 'Library name (used in require call)'
-  })
-  .option('client', {
-    alias: 'c',
-    type: 'string',
-    description: 'The path of client JavaScript file or project (if the specified path is a directory, all JavaScript files in the diretory will be executed)'
-  })
-  .option('mocha', {
-    type: 'boolean',
-    description: 'Client JavaScript file or project use mocha as the test framework'
-  })
-  .option('compress', {
-    type: 'boolean',
-    description: 'Whether to compress the model',
-    default: true
-  })
-  .option('output', {
-    alias: 'o',
-    type: 'string',
-    description: 'Output path for the generated model'
-  })
-  .demandOption(['library', 'client'])
-  .help().parse();
+    .usage('Generate an API model for a library based on the given client code').option('library', {
+        alias: 'l',
+        type: 'string',
+        description: 'Library name (used in require call)'
+    })
+    .option('client', {
+        alias: 'c',
+        type: 'string',
+        description: 'The path of client JavaScript file or project (if the specified path is a directory, all JavaScript files in the diretory will be executed)'
+    })
+    .option('mocha', {
+        type: 'boolean',
+        description: 'Client JavaScript file or project use mocha as the test framework'
+    })
+    .option('compress', {
+        type: 'boolean',
+        description: 'Whether to compress the model',
+        default: true
+    })
+    .option('output', {
+        alias: 'o',
+        type: 'string',
+        description: 'Output path for the generated model'
+    })
+    .demandOption(['library', 'client'])
+    .help().parse();
 
-(function() {
+(function () {
     let client = argv.client;
 
-    let pathTree = {p: null, callChildren: {},
+    let pathTree = {
+        p: null, callChildren: {},
         newChildren: {},
         argChildren: {},
         accessPropChildren: {},
@@ -52,12 +57,17 @@ let argv = yargs(hideBin(process.argv))
         requireChildren: {},
         type: null
     };
-    
+
     let order = 0;
     let rhoRelations = [];
 
-    let nativeModules = ['fs', 'net', 'os', 'http', 'https', 'crypto', 'buffer', 
-        'dns', 'events', 'assert', 'console', 'process', 'child_process', 'util'
+    let nativeModules = [
+        'assert', 'buffer', 'child_process', 'crypto',
+        'dns', 'events', 'fs', 'http',
+        'https', 'module', 'net', 'os',
+        'path', 'process', 'stream', 'tls',
+        'tty', 'url', 'util', 'vm',
+        'zlib'
     ];
 
 
@@ -66,45 +76,45 @@ let argv = yargs(hideBin(process.argv))
         for (let pathComp of path) {
             if (pathComp.compType === 'require') {
                 if (!t.requireChildren[pathComp.moduleName]) {
-                    t.requireChildren[pathComp.moduleName] = {p: pathComp, callChildren: {}, newChildren: {}, argChildren: {}, accessPropChildren: {}, writePropChildren: {}, type: type, order: order++};
+                    t.requireChildren[pathComp.moduleName] = { p: pathComp, callChildren: {}, newChildren: {}, argChildren: {}, accessPropChildren: {}, writePropChildren: {}, type: type, order: order++ };
                 }
                 t = t.requireChildren[pathComp.moduleName];
             }
 
             if (pathComp.compType === 'accessProp') {
                 if (!t.accessPropChildren[pathComp.propName]) {
-                    t.accessPropChildren[pathComp.propName] = {p: pathComp, callChildren: {}, newChildren: {}, argChildren: {}, accessPropChildren: {}, writePropChildren: {}, type: type, order: order++};
+                    t.accessPropChildren[pathComp.propName] = { p: pathComp, callChildren: {}, newChildren: {}, argChildren: {}, accessPropChildren: {}, writePropChildren: {}, type: type, order: order++ };
                 }
                 t = t.accessPropChildren[pathComp.propName];
             }
-            
+
             if (pathComp.compType === 'writeProp') {
                 if (!t.writePropChildren[pathComp.propName]) {
-                    t.writePropChildren[pathComp.propName] = {p: pathComp, callChildren: {}, newChildren: {}, argChildren: {}, accessPropChildren: {}, writePropChildren: {}, type: type, order: order++};
+                    t.writePropChildren[pathComp.propName] = { p: pathComp, callChildren: {}, newChildren: {}, argChildren: {}, accessPropChildren: {}, writePropChildren: {}, type: type, order: order++ };
                 }
                 t = t.writePropChildren[pathComp.propName];
             }
-            
+
             if (pathComp.compType === 'arg') {
                 if (!t.argChildren[pathComp.callId]) {
                     t.argChildren[pathComp.callId] = {};
                 }
                 if (!t.argChildren[pathComp.callId][pathComp.argId]) {
-                    t.argChildren[pathComp.callId][pathComp.argId] = {p: pathComp, callChildren: {}, newChildren: {}, argChildren: {}, accessPropChildren: {}, writePropChildren: {}, type: type, order: order++};
+                    t.argChildren[pathComp.callId][pathComp.argId] = { p: pathComp, callChildren: {}, newChildren: {}, argChildren: {}, accessPropChildren: {}, writePropChildren: {}, type: type, order: order++ };
                 }
                 t = t.argChildren[pathComp.callId][pathComp.argId];
             }
 
             if (pathComp.compType === 'call') {
                 if (!t.callChildren[pathComp.callId]) {
-                    t.callChildren[pathComp.callId] = {p: pathComp, callChildren: {}, newChildren: {}, argChildren: {}, accessPropChildren: {}, writePropChildren: {}, type: type, order: order++};
+                    t.callChildren[pathComp.callId] = { p: pathComp, callChildren: {}, newChildren: {}, argChildren: {}, accessPropChildren: {}, writePropChildren: {}, type: type, order: order++ };
                 }
                 t = t.callChildren[pathComp.callId];
             }
 
             if (pathComp.compType === 'new') {
                 if (!t.newChildren[pathComp.callId]) {
-                    t.newChildren[pathComp.callId] = {p: pathComp, callChildren: {}, newChildren: {}, argChildren: {}, accessPropChildren: {}, writePropChildren: {}, type: type, order: order++};
+                    t.newChildren[pathComp.callId] = { p: pathComp, callChildren: {}, newChildren: {}, argChildren: {}, accessPropChildren: {}, writePropChildren: {}, type: type, order: order++ };
                 }
                 t = t.newChildren[pathComp.callId];
             }
@@ -137,14 +147,14 @@ let argv = yargs(hideBin(process.argv))
             }
         }
 
-        
+
         delete t.callChildren[prefix[prefix.length - 1].callId];
     }
 
 
     function pathTreeToList(currentNode, accumulatedPath, allPaths) {
         if (currentNode.p !== null) {
-            allPaths.push({path: accumulatedPath.concat(currentNode.p), type: currentNode.type, order: currentNode.order})
+            allPaths.push({ path: accumulatedPath.concat(currentNode.p), type: currentNode.type, order: currentNode.order })
         }
         let ap = accumulatedPath.concat(currentNode.p === null ? [] : currentNode.p);
         if (currentNode.requireChildren) {
@@ -170,18 +180,18 @@ let argv = yargs(hideBin(process.argv))
         for (let c of Object.values(currentNode.writePropChildren)) {
             pathTreeToList(c, ap, allPaths);
         }
-        
+
     }
 
     function getProxy(obj, path) {
         let proxy = new Proxy(obj, {
-            get: function(target, p, receiver) {
-                // If the property is a Symbol, the corresponding value is directly returned since
-                // the original NoRegrets+ does not deal with Symbols.
+            get: function (target, p, receiver) {
+                /* If the property is a Symbol, the corresponding value is directly returned since
+                 the original NoRegrets+ does not deal with Symbols. */
                 if (typeof p !== 'string') {
                     return target[p];
                 }
-                // Return the access path for an object.
+                /* Return the access path for an object. */
                 if (p === '@@__PATH__@@') {
                     return path;
                 }
@@ -189,8 +199,8 @@ let argv = yargs(hideBin(process.argv))
                 if (typeof target[p] === 'function' && target[p].toString().indexOf('[native code]') >= 0) {
                     return target[p];
                 }
-                let newPath = path.concat([{compType: 'accessProp', propName: p}]);
-                let type = isCovariant(newPath)? getType(target[p]) : getArgumentType(target[p]);
+                let newPath = path.concat([{ compType: 'accessProp', propName: p }]);
+                let type = isCovariant(newPath) ? getType(target[p]) : getArgumentType(target[p]);
                 addPathAndType(newPath, type);
                 if (target[p] !== null && (type === 'object' || type === 'function')) {
                     return getProxy(target[p], newPath);
@@ -198,18 +208,20 @@ let argv = yargs(hideBin(process.argv))
                     return target[p];
                 }
             },
-            set: function(target, p, newValue) {
-                let newPath = path.concat([{compType: 'writeProp', propName: p}]);
+
+            set: function (target, p, newValue) {
+                let newPath = path.concat([{ compType: 'writeProp', propName: p }]);
                 let type = isCovariant(newPath) ? getType(newValue) : getArgumentType(newValue);
                 addPathAndType(newPath, type);
                 target[p] = newValue;
                 return true;
             },
-            apply: function(target, thisArg, argArray) {
+
+            apply: function (target, thisArg, argArray) {
                 let callId = makeRandomString(6);
                 let proxiedArgArray = [];
                 for (let i = 0; i < argArray.length; i++) {
-                    let newPath = path.concat([{compType: 'arg', callId: callId, argId: i}]);
+                    let newPath = path.concat([{ compType: 'arg', callId: callId, argId: i }]);
                     let type = isCovariant(newPath) ? getType(argArray[i]) : getArgumentType(argArray[i]);
                     addPathAndType(newPath, type);
                     if ((typeof argArray[i] === 'object' || typeof argArray[i] === 'function') && argArray[i] !== null) {
@@ -225,8 +237,8 @@ let argv = yargs(hideBin(process.argv))
                 }
 
                 let result = target.apply(thisArg, proxiedArgArray);
-                
-                let newPath = path.concat([{compType: 'call', callId: callId}]);
+
+                let newPath = path.concat([{ compType: 'call', callId: callId }]);
                 let type = isCovariant(newPath) ? getType(result) : getArgumentType(result);
                 addPathAndType(newPath, type);
                 if (result !== null && typeof result === 'object' && result['@@__PATH__@@']) {
@@ -238,16 +250,17 @@ let argv = yargs(hideBin(process.argv))
                     return result
                 }
             },
-            construct: function(target, argArray, newTarget) {
+
+            construct: function (target, argArray, newTarget) {
                 let callId = makeRandomString(6);
                 let proxiedArgArray = [];
                 for (let i = 0; i < argArray.length; i++) {
-                    let newPath = path.concat([{compType: 'arg', callId: callId, argId: i}]);
+                    let newPath = path.concat([{ compType: 'arg', callId: callId, argId: i }]);
                     let type = isCovariant(newPath) ? getType(argArray[i]) : getArgumentType(argArray[i]);
                     addPathAndType(newPath, type);
                     if (typeof argArray[i] === 'object' && argArray[i] !== null) {
                         proxiedArgArray.push(getProxy(argArray[i], newPath));
-                        
+
                         let argPath = argArray[i]['@@__PATH__@@'];
                         if (argPath) {
                             rhoRelations.push([argPath, newPath]);
@@ -255,15 +268,15 @@ let argv = yargs(hideBin(process.argv))
                     } else {
                         proxiedArgArray.push(argArray[i]);
                     }
-                    
+
                 }
                 let result = Reflect.construct(target, argArray);
-                let newPath = path.concat([{compType: 'new', callId: callId}]);
+                let newPath = path.concat([{ compType: 'new', callId: callId }]);
                 let type = isCovariant(newPath) ? getType(result) : getArgumentType(result);
                 addPathAndType(newPath, type);
                 return getProxy(result, newPath);
             }
-            
+
         });
         return proxy;
     }
@@ -300,8 +313,8 @@ let argv = yargs(hideBin(process.argv))
     }
 
     // function tryRemovePath(currentNode, accumulatedPath) {
-    //     for (let c of )
-        
+    //     TODO
+
     // }
 
     function compress() {
@@ -367,7 +380,7 @@ let argv = yargs(hideBin(process.argv))
     }
 
     function mockedRequire(moduleName) {
-        // For native modules, directly import them
+        /* For native modules, directly import them */
         if (moduleName.startsWith('node:')) {
             return require(moduleName);
         }
@@ -381,8 +394,8 @@ let argv = yargs(hideBin(process.argv))
 
         if (moduleName === argv.library) {
             let lib = require(modulePath);
-            let initPath = [{compType: 'require', moduleName: moduleName}]
-            let proxiedLib = getProxy(lib, initPath)
+            let initPath = [{ compType: 'require', moduleName: moduleName }]
+            let proxiedLib = getProxy(lib, initPath);
             let type = getType(lib);
             addPathAndType(initPath, type);
             return proxiedLib;
@@ -418,28 +431,53 @@ let argv = yargs(hideBin(process.argv))
 
     if (!argv.mocha) {
         javaScriptFiles.forEach(f => {
-            let content = fs.readFileSync(f, {encoding: 'utf-8'});
+            let content = fs.readFileSync(f, { encoding: 'utf-8' });
+            let ast = babelParser.parse(content);
+            babelTraverse(ast, {
+                exit(path) {
+                    if (path.isProgram()) {
+                        let funcExpr = babelTypes.functionExpression(
+                            null,
+                            [
+                                babelTypes.identifier('require'),
+                            ],
+                            babelTypes.blockStatement(
+                                path.node.body,
+                                path.node.directives
+                            )
+                        );
+                        path.node.body = [babelTypes.parenthesizedExpression(funcExpr)];
+                        path.node.directives = [];
+                        path.skip();
+                    }
+                }
+            });
+
+            content = babelGenerator(ast).code;
             try {
+                /* Now use Node.js vm APIs */
+                let compiledFunc = vm.runInNewContext(content, undefined, { filename: f });
+                compiledFunc.call(undefined, mockedRequire);
                 new Function('require', content)(mockedRequire);
             } catch (e) {
                 logger.info(`Encountered error in executing ${f}: ` + e);
             }
         });
-        
+
     } else {
         javaScriptFiles.forEach(f => {
-            let content = fs.readFileSync(f, {encoding: 'utf-8'});
+            let content = fs.readFileSync(f, { encoding: 'utf-8' });
             try {
                 new Function('require', 'describe', 'it', 'expect', 'assert', content)(
                     mockedRequire,
-                    function(message, cb) {
+                    function (message, cb) {
                         logger.info(`Executing: ${message}`);
                         cb();
                     },
-                    function(message, cb) {
+                    function (message, cb) {
                         logger.info(`Executing test: ${message}`);
                         if (cb.length === 1) {
-                            cb(function() {});
+                            cb(function () { });
                         } else {
                             cb();
                         }
@@ -455,7 +493,7 @@ let argv = yargs(hideBin(process.argv))
 
     if (argv.compress) {
         compress();
-    }   
+    }
 
     logger.info('Rho relations: ' + JSON.stringify(rhoRelations));
 
@@ -465,7 +503,7 @@ let argv = yargs(hideBin(process.argv))
         let allPaths = [];
         pathTreeToList(pathTree, [], allPaths);
         allPaths.sort((a, b) => a.order - b.order);
-        fs.writeFileSync(outputPath, JSON.stringify({paths: allPaths, rhoRelations: rhoRelations}, (k, v) => {
+        fs.writeFileSync(outputPath, JSON.stringify({ paths: allPaths, rhoRelations: rhoRelations }, (k, v) => {
             if (v === Infinity) {
                 return 'Infinity';
             }
